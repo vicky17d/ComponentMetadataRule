83b32bb9249de1c545a1ef66585e771fd42f8b54
vickyd <vicky17d@gmail.com>
2024-12-30 11:48:17 -0800

    Able to print attribute usage - runtime vs api

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
 }
 
 tasks.register("compareClasspaths") {
+    outputs.upToDateWhen { false }
+    // Depend on configurations to ensure they're resolved
+    dependsOn(configurations.compileClasspath)
+    dependsOn(configurations.runtimeClasspath)
     doLast {
         val compileFiles = configurations["compileClasspath"].files.map { it.nameWithoutExtension }.toSet()
         val runtimeFiles = configurations["runtimeClasspath"].files.map { it.nameWithoutExtension }.toSet()
--- buildSrc/src/main/kotlin/ExposeRuntimeDepsRule.kt
+++ buildSrc/src/main/kotlin/ExposeRuntimeDepsRule.kt
@@ -1,14 +1,17 @@
 import org.gradle.api.Action
 import org.gradle.api.artifacts.CacheableRule
 import org.gradle.api.artifacts.ComponentMetadataContext
+import org.gradle.api.artifacts.DirectDependenciesMetadata
+import org.gradle.api.attributes.AttributeContainer
 import org.gradle.api.artifacts.ComponentMetadataRule
 import org.gradle.api.attributes.Usage
+import org.gradle.api.attributes.Attribute
 import org.gradle.api.model.ObjectFactory
 import org.gradle.kotlin.dsl.named
 import javax.inject.Inject
 import org.gradle.api.artifacts.VariantMetadata
 
-@CacheableRule
+//@CacheableRule // comment or else rule does not print everytime
 abstract class ExposeRuntimeDepsRule @Inject constructor(
     private val objects: ObjectFactory
 ) : ComponentMetadataRule {
@@ -21,7 +24,20 @@ abstract class ExposeRuntimeDepsRule @Inject constructor(
 
         details.allVariants(object : Action<VariantMetadata> {
             override fun execute(variant: VariantMetadata) {
-                println(variant)
+                variant.attributes(object : Action<AttributeContainer> {
+                    override fun execute(attributes: AttributeContainer) {
+                        val usageAttribute = Attribute.of("org.gradle.usage", String::class.java)
+                        println("Variant: ${attributes.getAttribute(usageAttribute)}")
+                    }
+                })
+                println("  - Dependencies: ")
+                variant.withDependencies(object : Action<DirectDependenciesMetadata> {
+                    override fun execute(dependencies: DirectDependenciesMetadata) {
+                        dependencies.forEach { dep ->
+                            println("    - ${dep.group}:${dep.name}")
+                        }
+                    }
+                })
             }
         })
     }
